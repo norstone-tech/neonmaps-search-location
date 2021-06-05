e2ef7102196597cdb456c8c4c5b854e357b7e951
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2021-06-05 01:24:37 -0400

    make phase1 skipable

--- bin/neonmaps-location-indexer.js
+++ bin/neonmaps-location-indexer.js
@@ -35,14 +35,15 @@ const NANO_DIVISOR = 10 ** 9;
 const SIZE_BBOX_RATIO = 5;
 
 const options = program
-.requiredOption("-m, --map <path>", "Map file, in .osm.pbf format")
-.option(
-	"--no-ignore-tagless",
-	"Tag-less elements are ignored by default as they are assumed to be part of other geometry"
-)
-.option("--tmpdir <dir>", "(debug) temp folder to use during indexing")
-.parse()
-.opts();
+	.requiredOption("-m, --map <path>", "Map file, in .osm.pbf format")
+	.option(
+		"--no-ignore-tagless",
+		"Tag-less elements are ignored by default as they are assumed to be part of other geometry"
+	)
+	.option("--tmpdir <dir>", "(debug) temp folder to use during indexing")
+	.option("--no-phase1", "(debug) skip phase 1")
+	.parse()
+	.opts();
 
 const writeAndWait = function(
 	/**@type {fs.WriteStream}*/ stream,
@@ -62,169 +63,171 @@ const tmpFiles = new Map();
 		await fsp.mkdtemp(path.join(os.tmpdir(), "neonmaps-location-"));
 	try{
 		await mapReader.init();
-		/**@type {Promise<number>} */
-		let offsetPromise;
-		/**@type {Promise<OSMDecodeResult>} */
-		let mapDataPromise;
-		let curentSegment = 0;
-		let segmentCount = 0;
-		let firstSegmentWithWay = 0;
-		for(offsetPromise of mapReader.offsets()){
-			await offsetPromise;
-			segmentCount += 1;
-		}
-		for(let i = MIN_INDEX_GRANULARITY; i <= MAX_INDEX_GRANULARITY; i += 1){
-			const tmpFile = new TempFileArray(tmpDir, i);
-			tmpFiles.set(i, tmpFile);
-			await tmpFile.init();
-		}
-		for(mapDataPromise of mapReader.mapSegments()){
-			const tmpFile = tmpFiles.get(MIN_INDEX_GRANULARITY);
-			const mapData = await mapDataPromise;
-			if(!firstSegmentWithWay && mapData.ways.length){
-				firstSegmentWithWay = curentSegment;
+			if(options.phase1){
+			/**@type {Promise<number>} */
+			let offsetPromise;
+			/**@type {Promise<OSMDecodeResult>} */
+			let mapDataPromise;
+			let curentSegment = 0;
+			let segmentCount = 0;
+			let firstSegmentWithWay = 0;
+			for(offsetPromise of mapReader.offsets()){
+				await offsetPromise;
+				segmentCount += 1;
 			}
-			if(!mapData.nodes.length){
-				break;
+			for(let i = MIN_INDEX_GRANULARITY; i <= MAX_INDEX_GRANULARITY; i += 1){
+				const tmpFile = new TempFileArray(tmpDir, i);
+				tmpFiles.set(i, tmpFile);
+				await tmpFile.init();
 			}
-			for(let i = 0; i < mapData.nodes.length; i += 1){
-				const node = mapData.nodes[i];
-				if(options.ignoreTagless && node.tags.size == 0){
-					continue;
+			for(mapDataPromise of mapReader.mapSegments()){
+				const tmpFile = tmpFiles.get(MIN_INDEX_GRANULARITY);
+				const mapData = await mapDataPromise;
+				if(!firstSegmentWithWay && mapData.ways.length){
+					firstSegmentWithWay = curentSegment;
 				}
-				const lon = Math.round(node.lon * NANO_DIVISOR);
-				const lat = Math.round(node.lat * NANO_DIVISOR);
-				const granularityDivisor = 10 ** MIN_INDEX_GRANULARITY;
-				const granularityLon = Math.floor(node.lon / granularityDivisor);
-				const granularityLat = Math.floor(node.lat / granularityDivisor);
-				const uGranularityLon = granularityLon + (180 / granularityDivisor);
-				const uGranularityLat = granularityLat + (90 / granularityDivisor);
-				const searchNum = uGranularityLon * 10 ** ((-MIN_INDEX_GRANULARITY) + 3) + uGranularityLat;
-				await tmpFile.push({
-					searchNum,
-					searchType: SEARCH_TYPE_WITHIN,
-					id: node.id,
-					type: OSMTYPE_NODE,
-					lonMin: lon,
-					latMin: lat,
-					lonMax: lon,
-					latMax: lat,
-				});
-			}
-			curentSegment += 1;
-			logProgressMsg(
-				"Node resolving: " + curentSegment + "/" + segmentCount + " (" +
-				(curentSegment / segmentCount * 100).toFixed(2) +
-				"%)"
-			);
-		}
-		console.log("Node resolving: " + segmentCount + "/" + segmentCount + " (100%)");
-		curentSegment = 0;
-		
-		for(offsetPromise of mapReader.offsets()){
-			const offset = await offsetPromise;
-			curentSegment += 1;
-			if(curentSegment < firstSegmentWithWay){
-				continue;
-			}
-			const mapData = await mapReader.readDecodedMapSegment(offset);
-			const things = [...mapData.ways, ...mapData.relations];
-			for(let i = 0; i < things.length; i += 1){
-				const thing = things[i];
-				if(options.ignoreTagless && thing.tags.size == 0){
-					continue;
+				if(!mapData.nodes.length){
+					break;
 				}
-				const geometry = thing.type == "way" ?
-					await mapReader.getWayGeoJSON(thing) :
-					await mapReader.getRelationGeoJSON(thing);
-				const bbox = geoBBox(geometry);
-				const nanoBBox = bbox.map((v => Math.round(v * NANO_DIVISOR)));
-				// Math.abs is probably redundant here, but it's good to be safe
-				const bbSize = Math.max(Math.abs(bbox[0] - bbox[2]), Math.abs(bbox[1] - bbox[3]));
-				let exponent = Math.floor(Math.log10(bbSize * SIZE_BBOX_RATIO)) + 1;
-				if(exponent < MIN_INDEX_GRANULARITY){
-					exponent = MIN_INDEX_GRANULARITY;
-				}else if(exponent > MAX_INDEX_GRANULARITY){
-					exponent = MAX_INDEX_GRANULARITY;
-				}
-				const tmpFile = tmpFiles.get(exponent);
-				const granularityDivisor = 10 ** exponent;
-				const granularityMinLon = Math.floor(bbox[0] / granularityDivisor);
-				const granularityMinLat = Math.floor(bbox[1] / granularityDivisor);
-				const granularityMaxLon = Math.floor(bbox[2] / granularityDivisor);
-				const granularityMaxLat = Math.floor(bbox[3] / granularityDivisor);
-				/* These for loops are needed in case this element is actually intersecting with multiple granularity
-				indexes. This also conveniently skips over any elements with invalid geometry */
-				for(
-					let granularityLon = granularityMinLon;
-					granularityLon <= granularityMaxLon;
-					granularityLon += 1
-				){
+				for(let i = 0; i < mapData.nodes.length; i += 1){
+					const node = mapData.nodes[i];
+					if(options.ignoreTagless && node.tags.size == 0){
+						continue;
+					}
+					const lon = Math.round(node.lon * NANO_DIVISOR);
+					const lat = Math.round(node.lat * NANO_DIVISOR);
+					const granularityDivisor = 10 ** MIN_INDEX_GRANULARITY;
+					const granularityLon = Math.floor(node.lon / granularityDivisor);
+					const granularityLat = Math.floor(node.lat / granularityDivisor);
 					const uGranularityLon = granularityLon + (180 / granularityDivisor);
+					const uGranularityLat = granularityLat + (90 / granularityDivisor);
+					const searchNum = uGranularityLon * 10 ** ((-MIN_INDEX_GRANULARITY) + 3) + uGranularityLat;
+					await tmpFile.push({
+						searchNum,
+						searchType: SEARCH_TYPE_WITHIN,
+						id: node.id,
+						type: OSMTYPE_NODE,
+						lonMin: lon,
+						latMin: lat,
+						lonMax: lon,
+						latMax: lat,
+					});
+				}
+				curentSegment += 1;
+				logProgressMsg(
+					"Node resolving: " + curentSegment + "/" + segmentCount + " (" +
+					(curentSegment / segmentCount * 100).toFixed(2) +
+					"%)"
+				);
+			}
+			console.log("Node resolving: " + segmentCount + "/" + segmentCount + " (100%)");
+			curentSegment = 0;
+			
+			for(offsetPromise of mapReader.offsets()){
+				const offset = await offsetPromise;
+				curentSegment += 1;
+				if(curentSegment < firstSegmentWithWay){
+					continue;
+				}
+				const mapData = await mapReader.readDecodedMapSegment(offset);
+				const things = [...mapData.ways, ...mapData.relations];
+				for(let i = 0; i < things.length; i += 1){
+					const thing = things[i];
+					if(options.ignoreTagless && thing.tags.size == 0){
+						continue;
+					}
+					const geometry = thing.type == "way" ?
+						await mapReader.getWayGeoJSON(thing) :
+						await mapReader.getRelationGeoJSON(thing);
+					const bbox = geoBBox(geometry);
+					const nanoBBox = bbox.map((v => Math.round(v * NANO_DIVISOR)));
+					// Math.abs is probably redundant here, but it's good to be safe
+					const bbSize = Math.max(Math.abs(bbox[0] - bbox[2]), Math.abs(bbox[1] - bbox[3]));
+					let exponent = Math.floor(Math.log10(bbSize * SIZE_BBOX_RATIO)) + 1;
+					if(exponent < MIN_INDEX_GRANULARITY){
+						exponent = MIN_INDEX_GRANULARITY;
+					}else if(exponent > MAX_INDEX_GRANULARITY){
+						exponent = MAX_INDEX_GRANULARITY;
+					}
+					const tmpFile = tmpFiles.get(exponent);
+					const granularityDivisor = 10 ** exponent;
+					const granularityMinLon = Math.floor(bbox[0] / granularityDivisor);
+					const granularityMinLat = Math.floor(bbox[1] / granularityDivisor);
+					const granularityMaxLon = Math.floor(bbox[2] / granularityDivisor);
+					const granularityMaxLat = Math.floor(bbox[3] / granularityDivisor);
+					/* These for loops are needed in case this element is actually intersecting with multiple granularity
+					indexes. This also conveniently skips over any elements with invalid geometry */
 					for(
-						let granularityLat = granularityMinLat;
-						granularityLat <= granularityMaxLat;
-						granularityLat += 1
+						let granularityLon = granularityMinLon;
+						granularityLon <= granularityMaxLon;
+						granularityLon += 1
 					){
-						const uGranularityLat = granularityLat + (90 / granularityDivisor);
-						const searchBBoxPoly = geoBBoxPoly([
-							Number((
-								(granularityLon * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
-							).toFixed(9)),
-							Number((
-								(granularityLat * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
-							).toFixed(9)),
-							Number((
-								((granularityLon + 1) * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
-							).toFixed(9)),
-							Number((
-								((granularityLat + 1) * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
-							).toFixed(9))
-						]);
-						const searchType = geoBBoxContains(searchBBoxPoly.bbox, bbox) ?
-							SEARCH_TYPE_WITHIN : (
-								geoIntersects(searchBBoxPoly, geometry) ?
-									SEARCH_TYPE_INTERSECT : (
-										(
-											geometry.geometry.type !== "LineString" &&
-											geometry.geometry.type !== "MultiLineString" &&
-											multiPolyContainsPoly(geometry, searchBBoxPoly)
-										) ? SEARCH_TYPE_ENVELOPED : SEARCH_TYPE_INVALID
-									)
-							);
-						if(searchType == SEARCH_TYPE_INVALID){
-							continue;
+						const uGranularityLon = granularityLon + (180 / granularityDivisor);
+						for(
+							let granularityLat = granularityMinLat;
+							granularityLat <= granularityMaxLat;
+							granularityLat += 1
+						){
+							const uGranularityLat = granularityLat + (90 / granularityDivisor);
+							const searchBBoxPoly = geoBBoxPoly([
+								Number((
+									(granularityLon * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
+								).toFixed(9)),
+								Number((
+									(granularityLat * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
+								).toFixed(9)),
+								Number((
+									((granularityLon + 1) * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
+								).toFixed(9)),
+								Number((
+									((granularityLat + 1) * 10 ** (NANO_EXPONENT + exponent)) / NANO_DIVISOR
+								).toFixed(9))
+							]);
+							const searchType = geoBBoxContains(searchBBoxPoly.bbox, bbox) ?
+								SEARCH_TYPE_WITHIN : (
+									geoIntersects(searchBBoxPoly, geometry) ?
+										SEARCH_TYPE_INTERSECT : (
+											(
+												geometry.geometry.type !== "LineString" &&
+												geometry.geometry.type !== "MultiLineString" &&
+												multiPolyContainsPoly(geometry, searchBBoxPoly)
+											) ? SEARCH_TYPE_ENVELOPED : SEARCH_TYPE_INVALID
+										)
+								);
+							if(searchType == SEARCH_TYPE_INVALID){
+								continue;
+							}
+							const searchNum = uGranularityLon * 10 ** ((-exponent) + 3) + uGranularityLat;
+							await tmpFile.push({
+								searchNum,
+								searchType: SEARCH_TYPE_WITHIN,
+								id: thing.id,
+								type: thing.type == "way" ? OSMTYPE_WAY : OSMTYPE_RELATION,
+								lonMin: nanoBBox[0],
+								latMin: nanoBBox[1],
+								lonMax: nanoBBox[2],
+								latMax: nanoBBox[3],
+							});
 						}
-						const searchNum = uGranularityLon * 10 ** ((-exponent) + 3) + uGranularityLat;
-						await tmpFile.push({
-							searchNum,
-							searchType: SEARCH_TYPE_WITHIN,
-							id: thing.id,
-							type: thing.type == "way" ? OSMTYPE_WAY : OSMTYPE_RELATION,
-							lonMin: nanoBBox[0],
-							latMin: nanoBBox[1],
-							lonMax: nanoBBox[2],
-							latMax: nanoBBox[3],
-						});
 					}
 				}
+				logProgressMsg(
+					"way/relation resolving: " +
+						(curentSegment - firstSegmentWithWay) + "/" + (segmentCount - firstSegmentWithWay) +
+					" (" +
+					((curentSegment - firstSegmentWithWay) / (segmentCount - firstSegmentWithWay) * 100).toFixed(2) +
+					"%)"
+				);
 			}
-			logProgressMsg(
+			console.log(
 				"way/relation resolving: " +
-					(curentSegment - firstSegmentWithWay) + "/" + (segmentCount - firstSegmentWithWay) +
-				" (" +
-				((curentSegment - firstSegmentWithWay) / (segmentCount - firstSegmentWithWay) * 100).toFixed(2) +
-				"%)"
+				(segmentCount - firstSegmentWithWay) + "/" + (segmentCount - firstSegmentWithWay) +
+				" (100%)"
 			);
+			console.log("Index sorting: ?/? (0%)");
+			await Promise.all([...tmpFiles.values()].map(tmpFile => tmpFile.sort()));
+			console.log("Index sorting: ?/? (100%)");
 		}
-		console.log(
-			"way/relation resolving: " +
-			(segmentCount - firstSegmentWithWay) + "/" + (segmentCount - firstSegmentWithWay) +
-			" (100%)"
-		);
-		console.log("Index sorting: ?/? (0%)");
-		await Promise.all([...tmpFiles.values()].map(tmpFile => tmpFile.sort()));
-		console.log("Index sorting: ?/? (100%)");
 	}catch(ex){
 		console.error(ex);
 		process.exitCode = 1;
